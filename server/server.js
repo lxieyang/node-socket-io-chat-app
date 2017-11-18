6b08407cb29c638a73c4f5e97e2a3bb8ab477a33
lxieyang <lxieyang@umich.edu>
2017-11-18 15:28:40 -0500

    Great new users and alert others

--- server/server.js
+++ server/server.js
@@ -13,10 +13,30 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// io.emit emit to all connections; 
-// socket.emit send only to a single connection
+/*
+  Notes:
+  - io.emit emit to all connections; 
+  - socket.emit send only to a single connection
+  - socket.broadcast.emit send to everyone else connected
+*/
+
 io.on('connection', (socket) => {
   console.log('New user connected');
+  // a new user connected
+
+  // socket.emit welcome message to this user
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  // alert everyone else that someone is new here
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -33,5 +53,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on 3000 ${port}`);
+  console.log(`Server is up on ${port}`);
 });
